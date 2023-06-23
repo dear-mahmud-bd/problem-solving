b37ca412a682b7d79341b098b524de54aee0bc72
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2023-06-23 16:10:12 +0600

    day-17

---
+++ 01_Phase/Week-3/Day-7/024NumReplac.cpp
@@ -0,0 +1,34 @@
+// 
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    string canGetString() {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        string s;
+        cin >> s;
+        unordered_map<int, char> mp;
+        for (int i = 0; i < n; i++) {
+            if (mp.find(a[i]) == mp.end()) {
+                mp[a[i]] = s[i];
+            } else if (mp[a[i]] != s[i]) {
+                return "NO";
+            }
+        }
+        return "YES";
+    } 
+};
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        Solution ob;
+        cout<<ob.canGetString()<<"\n";
+    }
+    return 0;
+}
